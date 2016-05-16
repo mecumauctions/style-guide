3dbc748ab4d7311cb199643b8cc9001986022b6b
kurtcunningham <cunningham.kurt@gmail.com>
2016-05-16 17:11:14 -0500

    fixed weird js crossover issue

--- source/javascript/components/_navigation.js
+++ source/javascript/components/_navigation.js
@@ -1,25 +1,23 @@
 // Page Navigation Interactions
 
-var $navbarToggle = $('.navbar__link--toggle'),
+var $navbarToggle = $('.site__navigation .navbar__link--toggle'),
+    $navbarToggleExample = $('.page__navigation--example .navbar__link--toggle'),
     $navbarItem = $('.navbar__item'),
-    $submenu = $('.navbar__submenu');
+    $navbarItemExample = $('.page__navigation--example .navbar__item'),
+    $submenu = $('.navbar__submenu'),
+    $navbarItemActive = 'navbar__item--active',
+    $submenuActive = 'navbar__submenu--active',
+    $navigationIcon = $('.navbar__link--navigation-icon');
 
+
+// Site Navigation
 $navbarToggle.on('click', function (event) {
 
-  var $navbarItemActive = 'navbar__item--active',
-      $submenuActive = 'navbar__submenu--active',
-      $navigationIcon = $('.navbar__link--navigation-icon');
-
   event.preventDefault();
+  event.stopPropagation();
 
-  $navbarItem.removeClass($navbarItemActive);
-  $(this).parent($navbarItem).addClass($navbarItemActive);
-
-  $submenu.removeClass($submenuActive);
-  $(this).siblings($submenu).addClass($submenuActive);
-
-  $navigationIcon.removeClass('navbar__link--navigation-icon--active');
-  $(this).closest($navigationIcon).addClass('navbar__link--navigation-icon--active');
+  $(this).parent($navbarItem).toggleClass($navbarItemActive);
+  $(this).siblings($submenu).toggleClass($submenuActive);
 
   if($navbarItem.hasClass($navbarItemActive)) {
     $(this).siblings($submenu).attr('aria-hidden', 'false');
@@ -27,4 +25,18 @@ $navbarToggle.on('click', function (event) {
     $(this).siblings($submenu).attr('aria-hidden', 'true');
   };
 
+});
+
+
+
+// Module Navigation
+$navbarToggleExample.on('click', function (event) {
+
+  event.preventDefault();
+
+  $navbarItemExample.removeClass($navbarItemActive);
+  $(this).parent($navbarItemExample).addClass($navbarItemActive);
+
+  $navigationIcon.removeClass('navbar__link--navigation-icon--active');
+  $(this).closest($navigationIcon).addClass('navbar__link--navigation-icon--active');
 });
